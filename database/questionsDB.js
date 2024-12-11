eeb71df640db87dc7f44bb18f8f26d1da857cc54
sheikhfaris86 <sheikhfaris86@gmail.com>
2024-12-11 22:01:03 +0530

    added a apache 2.0 license

--- database/questionsDB.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  question: String,
-});
-
-const questionsModel = mongoose.model("questions_collections", userSchema);
-
-export default questionsModel;
